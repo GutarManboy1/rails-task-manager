1a04a6c31d2b911b758c7d14fbf0cbc5c59d70f0
Glenn Torrens <glenntorrens@gmail.com>
2024-02-13 17:46:28 +0900

    did somethig

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,9 @@
 class TasksController < ApplicationController
-  def add
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_033601) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_13_050019) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
-    t.text "details", 
+    t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
